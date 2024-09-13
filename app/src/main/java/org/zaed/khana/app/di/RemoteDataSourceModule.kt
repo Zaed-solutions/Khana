53418dfcda767a87589573294dc16817f8549b17
edrees <muhammed.edrees101@gmail.com>
2024-09-13 18:19:50 +0300

    feat/#1/implement_home_screen: Added some koin dependencies

--- app/src/main/java/org/zaed/khana/app/di/RemoteDataSourceModule.kt
+++ app/src/main/java/org/zaed/khana/app/di/RemoteDataSourceModule.kt
@@ -1,7 +1,38 @@
 package org.zaed.khana.app.di
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
+import org.zaed.khana.data.source.remote.AdvertisementRemoteDataSource
+import org.zaed.khana.data.source.remote.AdvertisementRemoteDataSourceImpl
+import org.zaed.khana.data.source.remote.CategoryRemoteDataSource
+import org.zaed.khana.data.source.remote.CategoryRemoteDataSourceImpl
+import org.zaed.khana.data.source.remote.ProductRemoteDataSource
+import org.zaed.khana.data.source.remote.ProductRemoteDataSourceImpl
+import kotlin.time.Duration.Companion.seconds
 
 val remoteDataSourceModule = module {
-
+    single<AdvertisementRemoteDataSource>{AdvertisementRemoteDataSourceImpl(get())}
+    single<CategoryRemoteDataSource>{CategoryRemoteDataSourceImpl(get())}
+    single<ProductRemoteDataSource>{ProductRemoteDataSourceImpl(get())}
+    single<HttpClient>{
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    useAlternativeNames = false
+                    isLenient = true
+                    encodeDefaults = true
+                })
+            }
+            install(WebSockets) {
+                pingInterval = 30.seconds.inWholeMilliseconds
+                contentConverter = KotlinxWebsocketSerializationConverter(Json)
+            }
+        }
+    }
 }
--- app/src/main/java/org/zaed/khana/app/di/ViewModelModule.kt
+++ app/src/main/java/org/zaed/khana/app/di/ViewModelModule.kt
@@ -1,7 +1,9 @@
 package org.zaed.khana.app.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import org.zaed.khana.presentation.home.HomeViewModel
 
 val viewModelModule = module {
-
+    viewModel { HomeViewModel(get(), get(), get()) }
 }
