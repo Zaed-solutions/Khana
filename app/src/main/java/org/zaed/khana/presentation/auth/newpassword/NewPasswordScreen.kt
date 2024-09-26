329ac503932947e092a3223e2b74bff99ac91697
Mohamed_Dev <mohamedzarea29@gmail.com>
2024-09-26 21:48:16 +0300

    feat/#2/implement_user_authentication: add new password screen

---
+++ app/src/main/java/org/zaed/khana/presentation/auth/newpassword/NewPasswordScreen.kt
@@ -0,0 +1,126 @@
+package org.zaed.khana.presentation.auth.newpassword
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.koinViewModel
+import org.zaed.khana.R
+
+@Composable
+fun NewPasswordScreen(
+    viewModel: NewPasswordViewModel = koinViewModel()
+) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+    NewPasswordScreenContent(
+        state = state,
+        action = viewModel::handleUiState
+    )
+}
+
+@Composable
+fun NewPasswordScreenContent(
+    state: NewPasswordUiState = NewPasswordUiState(),
+    action: (NewPasswordUIAction) -> Unit ={}
+) {
+    var passwordVisible by remember { mutableStateOf(false) }
+    var confirmPasswordVisible by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "New Password", fontSize = 24.sp)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = stringResource(R.string.your_new_password_must_be_different_from_previously_used_passwords),
+            fontSize = 14.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Password Input
+        OutlinedTextField(
+            value = state.password,
+            onValueChange = { action(NewPasswordUIAction.OnPasswordChanged(it)) },
+            label = { Text("Password") },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, contentDescription = "Toggle password visibility")
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Confirm Password Input
+        OutlinedTextField(
+            value = state.confirmPassword,
+            onValueChange = { action(NewPasswordUIAction.OnConfirmPasswordChanged(it)) },
+            label = { Text("Confirm Password") },
+            visualTransformation = if (confirmPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (confirmPasswordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                IconButton(onClick = { confirmPasswordVisible = !confirmPasswordVisible }) {
+                    Icon(
+                        imageVector = image,
+                        contentDescription = "Toggle confirm password visibility"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        // Create New Password Button
+        Button(
+            onClick = { action(NewPasswordUIAction.OnCreateNewPasswordClicked) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+        ) {
+            Text(text = "Create New Password", color = Color.White, fontSize = 16.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewPasswordScreenPreview() {
+    NewPasswordScreenContent()
+}
