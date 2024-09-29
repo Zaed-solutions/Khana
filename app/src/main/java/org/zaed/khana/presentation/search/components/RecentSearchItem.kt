54ceacbf2251c27bea9234e65427a04192e8ed29
edrees <muhammed.edrees101@gmail.com>
2024-09-29 16:50:28 +0300

    feat/#12/implement_search_screen: Implemented recent searches ui

---
+++ app/src/main/java/org/zaed/khana/presentation/search/components/RecentSearchItem.kt
@@ -0,0 +1,62 @@
+package org.zaed.khana.presentation.search.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.zaed.khana.presentation.theme.KhanaTheme
+
+@Composable
+fun RecentSearchItem(
+    modifier: Modifier = Modifier,
+    item: String,
+    onItemClick: (String) -> Unit,
+    onDeleteItem: (String) -> Unit,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(item) }
+    ) {
+        Text(
+            text = item,
+            style = MaterialTheme.typography.bodyMedium,
+            maxLines = 1,
+            modifier = Modifier
+                .weight(1f)
+                .focusable(false)
+        )
+        IconButton(
+            onClick = { onDeleteItem(item) }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Cancel,
+                contentDescription = "Delete",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun RecentSearchItemPreview() {
+    KhanaTheme {
+        RecentSearchItem(
+            item = "Chicken",
+            onItemClick = {},
+            onDeleteItem = {},
+        )
+    }
+}
---
+++ app/src/main/java/org/zaed/khana/presentation/search/components/RecentSearchesSection.kt
@@ -0,0 +1,83 @@
+package org.zaed.khana.presentation.search.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.zaed.khana.presentation.theme.KhanaTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun RecentSearchesSection(
+    modifier: Modifier = Modifier,
+    items: List<String>,
+    onItemClick: (String) -> Unit,
+    onDeleteItem: (String) -> Unit,
+    onClearAllRecentSearches: () -> Unit
+) {
+    if (items.isNotEmpty()) {
+        LazyColumn(
+            modifier = modifier.padding(horizontal = 16.dp),
+            contentPadding = PaddingValues(vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            stickyHeader {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Recent",
+                            style = MaterialTheme.typography.titleLarge,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            modifier = Modifier.weight(1f)
+                        )
+                        TextButton(onClick = { onClearAllRecentSearches() }) {
+                            Text(text = "Clear All", style = MaterialTheme.typography.titleLarge)
+                        }
+                    }
+                    HorizontalDivider(thickness = 1.dp)
+                }
+
+            }
+            items(items) { item ->
+                RecentSearchItem(
+                    modifier = Modifier.animateItem(),
+                    item = item,
+                    onItemClick = onItemClick,
+                    onDeleteItem = onDeleteItem
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun RecentSearchesPreview() {
+    KhanaTheme {
+        RecentSearchesSection(
+            items = listOf("Chicken", "Beef", "Pasta"),
+            onItemClick = {},
+            onDeleteItem = {},
+            onClearAllRecentSearches = {}
+        )
+    }
+}
