9693aaba9f182735c40a0c23c0d33c0f822131d2
edrees <muhammed.edrees101@gmail.com>
2024-10-15 20:44:23 +0300

    feat/#28/implement_leave_review_screen: Update product model to match the server

--- app/src/main/java/org/zaed/khana/data/model/Product.kt
+++ app/src/main/java/org/zaed/khana/data/model/Product.kt
@@ -1,12 +1,15 @@
 package org.zaed.khana.data.model
 
 import kotlinx.serialization.Serializable
+import org.zaed.khana.data.util.BrandFilterOption
+import org.zaed.khana.data.util.GenderFilterOption
 
 @Serializable
 data class Product(
     val id: String = "",
     val name: String = "",
     val rating: Float = 0f,
+    val reviewCount: Int = 0,
     val thumbnailImageLink: String = "",
     val previewImagesLinks: List<String> = emptyList(),
     val category: Category = Category(),
@@ -15,4 +18,7 @@ data class Product(
     val availableColors: List<Color> = emptyList(),
     val basePrice: Float = 0f,
     val isAvailable: Boolean = true,
+    val purchaseCount: Int = 0,
+    val brand: String = BrandFilterOption.ALL.displayName,
+    val gender: String = GenderFilterOption.MEN.displayName
 )
