e0471debc52f1d1d05b4a5738efe25f48a4fc994
edrees <muhammed.edrees101@gmail.com>
2024-10-16 22:59:38 +0300

    feat/#29/implement_track_order_screen: Updated order model

--- app/src/main/java/org/zaed/khana/data/model/Order.kt
+++ app/src/main/java/org/zaed/khana/data/model/Order.kt
@@ -1,5 +1,6 @@
 package org.zaed.khana.data.model
 
+import kotlinx.datetime.Clock
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,8 +13,10 @@ data class Order(
     val paymentStatus: String = PaymentStatus.NOT_SET.name,
     val orderStatus: String = OrderStatus.AWAITING_CONFIRMATION.name,
     val totalPrice: Float = 0f,
-    val expectedDeliveryEpochSeconds: Long = 0,
     val trackingId: String = "",
+    val expectedDeliveryEpochSeconds: Long = 0,
+    val createdAtEpochSeconds: Long = Clock.System.now().epochSeconds,
+    val cancelledEpochSeconds: Long = 0,
     val confirmedEpochSeconds: Long = 0,
     val shippedEpochSeconds: Long = 0,
     val deliveredEpochSeconds: Long = 0,
