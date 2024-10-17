bf81d0e7c9ef1c807b52ad9bcb72384bf7b342d5
edrees <muhammed.edrees101@gmail.com>
2024-10-17 22:14:01 +0300

    feat/#33/implement_help_center: solver merge conflict

--- app/src/main/java/org/zaed/khana/app/di/ViewModelModule.kt
+++ app/src/main/java/org/zaed/khana/app/di/ViewModelModule.kt
@@ -7,8 +7,6 @@ import org.zaed.khana.presentation.auth.login.LoginViewModel
 import org.zaed.khana.presentation.auth.otp.OtpViewModel
 import org.zaed.khana.presentation.auth.signup.SignUpViewModel
 import org.zaed.khana.presentation.cart.CartViewModel
-import org.zaed.khana.presentation.coupons.CouponsViewModel
-import org.zaed.khana.presentation.search.SearchViewModel
 import org.zaed.khana.presentation.category.CategoryViewModel
 import org.zaed.khana.presentation.checkout.CheckoutViewModel
 import org.zaed.khana.presentation.coupons.CouponsViewModel
